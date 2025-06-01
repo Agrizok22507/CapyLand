e09f5ba3b532f9ffdbe984b0cb4dd4848777bf67
Capybara <matusha_17@mail.ru>
2025-06-02 03:00:52 +0400

    Create Windux.py
    Начальный проект терминала Linux на Windows

---
+++ Windux.py
@@ -0,0 +1,109 @@
+import os
+from rich import print
+
+user = os.getlogin()
+capy = """⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣬⠷⣶⡖⠲⡄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⠶⠋⠁⠀⠸⣿⡀⠀⡁⠈⠙⠢⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⠞⠁⠀⠀⠀⠀⠀⠉⠣⠬⢧⠀⠀⠀⠀⠈⠻⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⡴⠃⠀⠀⢠⣴⣿⡿⠀⠀⠀⠐⠋⠀⠀⠀⠀⠀⠀⠘⠿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⡴⠋⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠒⠒⠓⠛⠓⠶⠶⢄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢠⠎⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠦⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⡞⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⢷⡄⠀⠀⠀⠀⠀⠀
+⢻⣇⣹⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀
+⠀⠻⣟⠋⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣄⠀⠀⠀
+⠀⠀⠀⠉⠓⠒⠊⠉⠉⢸⡙⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠘⣆⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣱⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣿⠀⠀⠀⠀⠀⢻⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⣧⡀⠀⠀⢀⡄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠇⠀⠀⠀⠀⠀⠀⢣⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⡧⢿⡀⠚⠿⢻⡆⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠘⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀⠀⠈⢹⡀⠀⠀⠀⠀⣾⡆⠀⠀⠀⠀⠀⠀⠀⠀⠾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠨⢷⣾⠀⠸⡷⠀⠀⠀⠘⡿⠂⠀⠀⠀⢀⡴⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⠳⢼⣧⡀⠀⠀⢶⡼⠦⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⡎⣽⠿⣦⣽⣷⠿⠒⠀⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠁⣴⠃⡿⠀⠀⢠⠆⠢⡀⠀⠀⠀⠈⢧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣠⠏⠀⣸⢰⡇⠀⢠⠏⠀⠀⠘⢦⣀⣀⠀⢀⠙⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠾⠿⢯⣤⣆⣤⣯⠼⠀⠀⢸⠀⠀⠀⠀⠀⣉⠭⠿⠛⠛⠚⠟⡇⠀⠀⣀⠀⢀⡤⠊⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⢸⣷⣶⣤⣦⡼⠀⠀⠀⣴⣯⠇⡀⣀⣀⠤⠤⠖⠁⠐⠚⠛⠉⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⠁⢋⡀⠀⠀⠀⠀⣛⣛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+
+def cmd_():
+    if cmd == "exp":
+        os.system("explorer")
+    elif cmd == "hi":
+        print(f"[bold violet]Hi {user}! Thanks for using Windux![/bold violet]")
+    elif cmd == "capy":
+        print(f"[bold violet]{capy}[/bold violet]")
+        print("[bold violet]Capybara![/bold violet]")
+    elif cmd == "calc":
+        os.system("calc")
+    elif cmd == "mkdir":
+        dir = input("   mkdir.")
+        try:
+            os.mkdir(dir)
+        except Exception as e:
+            print(f"[bold violet]w.{e}[/bold violet]")
+    elif cmd == "dir":
+        dir_ = input("  dir.")
+        items = os.listdir(dir_)
+        for item in items:
+            if os.path.isdir(item):
+                print(f"        {item}")
+            else:
+                print(f"        {item}")
+    elif cmd == "go":
+        go = input("  go.")
+        os.system(f"start {go}")
+    elif cmd == "c_fast":
+        calc_fast = input("   c_fast.")
+        yn = input(f"You are sure of '{calc_fast}' this go to powersell(y/n)")
+        if yn == "y":
+            try:
+                os.system(f"powershell {calc_fast}")
+            except Exception as e:
+                print(f"w.{e}")
+        elif yn == "n":
+            print("[bold violet]Ok![/bold violet]")
+        else:
+            print("[bold red]Incorrect comand...[/bold red]")
+    elif cmd == "clear":
+        os.startfile(filepath="Windux.py")
+    elif cmd == "pwrshl":
+        pwr = input("   pwr.")
+        yn_ = input(f"You are sure of '{pwr}'(y/n)")
+        if yn_ == "y":
+            try:
+                os.system(f"powershell {pwr}")
+            except Exception as e:
+                print(f"w.{e}")
+        elif yn_ == "n":
+            print("[bold violet]Ok![/bold violet]")
+        else:
+            print("[bold red]Incorrect comand...[/bold red]")
+    elif cmd == "help":
+        print("[bold violet].exp    -  explorer.exe[/bold violet]")
+        print("[bold violet].hi     -  hi[/bold violet]")
+        print("[bold violet].capy   -  Capybara!!![/bold violet]")
+        print("[bold violet].calc   -  calculator.exe[/bold violet]")
+        print("[bold violet].mkdir  -  make dir[/bold violet]")
+        print("[bold violet].dir    -  show dir[/bold violet]")
+        print("[bold violet].go     -  open file[/bold violet]")
+        print("[bold violet].pwrshl -  comand to powershell[/bold violet]")
+        print("[bold violet].c_fast -  calculator fast[/bold violet]")
+        print("[bold violet].clear  -  clear[/bold violet]")
+        print("[bold violet].help   -  show help text(this)[/bold violet]")
+    else:
+        yn = input(f"You are sure of '{cmd}'(y/n)")
+        if yn == "y":
+            try:
+                os.system(cmd)
+            except Exception as e:
+                print(f"[bold violet]w.{e}[/bold violet]")
+        elif yn == "n":
+            print("[bold violet]Ok![/bold violet]")
+        else:
+            print("[bold red]Incorrect comand...[/bold red]")
+
+while True:
+    cmd = input(".")
+    cmd_()
